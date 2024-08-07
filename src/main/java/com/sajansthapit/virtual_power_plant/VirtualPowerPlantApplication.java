4e08be05ac6c7c4feb5f96e0ae2a3dda9b3fbb7d
Sajan Sthapit <sajansthapit99@gmail.com>
2024-08-07 20:56:03 +0545

    inital project setup

---
+++ src/main/java/com/sajansthapit/virtual_power_plant/VirtualPowerPlantApplication.java
@@ -0,0 +1,13 @@
+package com.sajansthapit.virtual_power_plant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPowerPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPowerPlantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajansthapit/virtual_power_plant/VirtualPowerPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajansthapit.virtual_power_plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPowerPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
