a96ca21daa740148071ab119bd9248e4cfae330e
Sajan Sthapit <sajansthapit99@gmail.com>
2024-08-08 09:12:18 +0545

    add validation in saveAll

--- src/main/java/com/sajansthapit/virtual_power_plant/dto/request/BatteryRequest.java
+++ src/main/java/com/sajansthapit/virtual_power_plant/dto/request/BatteryRequest.java
@@ -1,1 +1,1 @@
-package com.sajansthapit.virtual_power_plant.dto.request;import com.sajansthapit.virtual_power_plant.dto.BatteryDto;import java.util.List;public record BatteryRequest(        List<BatteryDto> batteries) {}
+package com.sajansthapit.virtual_power_plant.dto.request;import com.sajansthapit.virtual_power_plant.dto.BatteryDto;import jakarta.validation.Valid;import jakarta.validation.constraints.NotEmpty;import java.util.List;public record BatteryRequest(        @NotEmpty(message = "Batteries List can't be empty")        List<@Valid BatteryDto> batteries) {}
